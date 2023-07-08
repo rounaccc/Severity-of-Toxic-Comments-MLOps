526efe86563362d89510d99aea23c75916d56cc9
Rounak <rounakbachwani@gmail.com>
2023-07-08 12:35:53 +0530

    Requirements and installation done

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Severity-of-Toxic-Comments-End-to-End"
+AUTHOR_USER_NAME = "rounaccc"
+SRC_REPO = "SeverityOfToxicCommentsEndToEnd"
+AUTHOR_EMAIL = "rounakbachwani@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="End to End project for Toxic Comment Classification",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
